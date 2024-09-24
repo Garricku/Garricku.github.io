ff1478cc6f67208dab616aa4b29710b76be47d53
Garrick Larson Potts <ricki.glp@gmail.com>
2024-09-24 19:53:18 +0200

    Header componeent

---
+++ one-piece-gallery-app/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+const Header = ({ toggleTheme }) => {
+  return (
+    <header className="header">
+      <img src="/images/one-piece-crew.jpg" alt="One Piece Crew" className="header-image" />
+      <nav className="navbar">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+      <button onClick={toggleTheme} className="theme-toggle-button">Mode</button>
+    </header>
+  );
+};
+
+export default Header;
