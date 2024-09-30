07401c2ac74c3650cf1dc70a4a85f7b98d89f373
Garrick Larson Potts <ricki.glp@gmail.com>
2024-09-30 17:23:50 +0200

    rearranged the header nav bar

--- one-piece-gallery-app/src/components/Header.jsx
+++ one-piece-gallery-app/src/components/Header.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './Header.css';
-import onePieceImage from '../images/one-piece-crew.jpg';
 
 const Header = ({ toggleTheme }) => {
   return (
     <header className="header">
-      <img src={ onePieceImage } alt="One Piece Crew" className="header-image" />
+      <img src="/images/one-piece-crew.jpg" alt="One Piece Crew" className="header-image" />
       <nav className="navbar">
         <ul>
           <li><a href="#home">Home</a></li>
-          <li><a href="#gallery">Gallery</a></li>
           <li><a href="#about">About</a></li>
+          <li><a href="https://your-quiz-website-url.com" target="_blank" rel="noopener noreferrer">Quiz</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
-      <button onClick={toggleTheme} className="theme-toggle-button">Mode</button>
+      <button onClick={toggleTheme} className="theme-toggle-button">Toggle Theme</button>
     </header>
   );
 };
