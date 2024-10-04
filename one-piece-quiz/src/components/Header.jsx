e93fa7f9208dc6d81d9516bc090a2279b4ffc3a3
Garrick Larson Potts <ricki.glp@gmail.com>
2024-10-04 17:39:12 +0200

    header img back and edit to nav

--- one-piece-quiz/src/components/Header.jsx
+++ one-piece-quiz/src/components/Header.jsx
@@ -5,13 +5,12 @@ import logo from '../images/one-piece-logo.png';
 const Header = ({ toggleTheme }) => {
   return (
     <header className="header">
-      <img src={ logo } alt="One Piece Crew" className="header-image" />
-      <nav className="navbar">
-        <ul>
-        <li><a href="#home">Home</a></li>
-        </ul>
-      </nav>
-      <button onClick={toggleTheme} className="theme-toggle-button">Toggle Theme</button>
+      <div>
+        <img src={ logo } alt="One Piece Crew" className="header-image" />
+      </div>
+      <div className='theme-butt'>
+        <button onClick={toggleTheme} className="theme-toggle-button">Toggle Theme</button>
+      </div>
     </header>
   );
 };
