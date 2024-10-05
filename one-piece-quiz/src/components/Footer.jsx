f0c691c270b3fd8f9ca4d71d2bdff8690f4c3370
Garrick Larson Potts <ricki.glp@gmail.com>
2024-10-05 05:31:57 +0200

    update for good adsense

--- one-piece-quiz/src/components/Footer.jsx
+++ one-piece-quiz/src/components/Footer.jsx
@@ -8,13 +8,6 @@ const Footer = () => {
         <a href="/terms-of-service.md" download>Terms of Service</a>
         <a href="/privacy-policy.md" download>Privacy Policy</a>
         <a href="mailto:contact@phoenixcreations.com">Contact Us</a>
-        <a href="/about-us">About Us</a>
-        <a href="/sitemap">Site Map</a>
-      </div>
-      <div className="footer-social">
-        <a href="https://www.facebook.com/phoenixcreations" target="_blank" rel="noopener noreferrer">Facebook</a>
-        <a href="https://www.twitter.com/phoenixcreations" target="_blank" rel="noopener noreferrer">Twitter</a>
-        <a href="https://www.instagram.com/phoenixcreations" target="_blank" rel="noopener noreferrer">Instagram</a>
       </div>
       <p>&copy; 2024 Phoenix Creations. All rights reserved.</p>
     </footer>
